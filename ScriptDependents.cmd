7867269b39f5dcf7422372f276d40470945d7e67
PowerWordTree <PowerWordTree@Gmail.com>
2024-01-02 22:38:16 +0800

    新增选项 Folder

--- ScriptDependents.cmd
+++ ScriptDependents.cmd
@@ -1,10 +1,9 @@
 ::获取脚本依赖
 ::@author FB
-::@version 1.1.1
+::@version 1.2.0
 
 ::Bin:Dumpbin.exe::
 ::Bin:Link.exe::
-::Script:Object.Destroy.CMD::
 ::Script:Config.FileRead.CMD::
 
 ::初始化环境
@@ -17,7 +16,9 @@ CD /D "%~dp0"
 CALL Config.FileRead.CMD "_CONFIG" "%~n0.ini"
 ::初始化配置
 FOR /F "tokens=1,* usebackq delims==" %%A IN (`SET "_CONFIG." 2^>NUL`) DO (
+  ::::展开变量
   CALL SET "%%~A=%%~B"
+  ::::创建目录
   IF /I "%%~xA" == ".DST" (
     IF /I "%_CONFIG.CLEAN%" == "TRUE" (
       CALL RMDIR /Q /S "%%~B" 1>NUL 2>&1
@@ -38,46 +39,53 @@ EXIT /B %_EXIT_CODE%
 FOR /F "tokens=1,2 usebackq delims=#/;: " %%A IN (
   `TYPE "%~1" ^| FINDSTR "::[^:][^:]*:[^:][^:]*::"`
 ) DO IF /I "%%~A" == "Bin" (
-  CALL :COPY_BIN "%%~B" && CALL :BIN "%_CONFIG.BIN.DST%\%%~B" || SET /A "_EXIT_CODE+=1"
+  CALL :COPY_BIN "%%~B" && CALL :BIN "%_CONFIG.BIN.DST%\%%~B"
 ) ELSE IF /I "%%~A" == "Script" (
-  CALL :COPY_SCRIPT "%%~B" && CALL :SCRIPT "%_CONFIG.SCRIPT.DST%\%%~B" || SET /A "_EXIT_CODE+=1"
-) ELSE IF /I "%%~A" == "Other" (
-  CALL :COPY_OTHER "%%~B" || SET /A "_EXIT_CODE+=1"
+  CALL :COPY_SCRIPT "%%~B" && CALL :SCRIPT "%_CONFIG.SCRIPT.DST%\%%~B"
+) ELSE IF /I "%%~A" == "File" (
+  CALL :COPY_FILE "%%~B"
+) ELSE IF /I "%%~A" == "Folder" (
+  CALL :COPY_FOLDER "%%~B"
 )
 EXIT /B
-
-::复制Script文件
-:COPY_SCRIPT
-IF NOT EXIST "%_CONFIG.SCRIPT.DST%\%~1" (
-  ECHO %~1
-  COPY "%~$_CONFIG.SCRIPT.SRC:1" "%_CONFIG.SCRIPT.DST%\%~1"
-  EXIT /B
-)
-EXIT /B 1
 
 ::扫描Bin依赖(递归调用)
 :BIN
 FOR /F "tokens=* usebackq" %%A IN (
   `Dumpbin.exe /DEPENDENTS "%~1" ^| FINDSTR /I /R /C:"^    [^ ].*\.DLL$"`
 ) DO IF EXIST "%%~$_CONFIG.BIN.SRC:A" (
-  CALL :COPY_BIN "%%~A" && CALL :BIN "%_CONFIG.BIN.DST%\%%~A" || SET /A "_EXIT_CODE+=1"
+  CALL :COPY_BIN "%%~A" && CALL :BIN "%_CONFIG.BIN.DST%\%%~A"
 )
+EXIT /B
+
+::复制Script文件
+:COPY_SCRIPT
+IF EXIST "%_CONFIG.SCRIPT.DST%\%~1" EXIT /B 1
+ECHO =^> %~1
+COPY /Y "%~$_CONFIG.SCRIPT.SRC:1" "%_CONFIG.SCRIPT.DST%\%~1" ^
+  || SET /A "_EXIT_CODE+=1"
 EXIT /B
 
 ::复制Bin文件
 :COPY_BIN
-IF NOT EXIST "%_CONFIG.BIN.DST%\%~1" (
-  ECHO %~1
-  COPY "%~$_CONFIG.BIN.SRC:1" "%_CONFIG.BIN.DST%\%~1"
-  EXIT /B
-)
-EXIT /B 1
+IF EXIST "%_CONFIG.BIN.DST%\%~1" EXIT /B 1
+ECHO =^> %~1
+COPY /Y "%~$_CONFIG.BIN.SRC:1" "%_CONFIG.BIN.DST%\%~1" ^
+  || SET /A "_EXIT_CODE+=1"
+EXIT /B
 
-::复制Other文件
-:COPY_OTHER
-IF NOT EXIST "%_CONFIG.OTHER.DST%\%~1" (
-  ECHO %~1
-  COPY "%~$_CONFIG.OTHER.SRC:1" "%_CONFIG.OTHER.DST%\%~1"
-  EXIT /B
-)
-EXIT /B 1
+::复制FILE文件
+:COPY_FILE
+IF EXIST "%_CONFIG.FILE.DST%\%~1" EXIT /B 1
+ECHO =^> %~1
+COPY /Y "%~$_CONFIG.FILE.SRC:1" "%_CONFIG.FILE.DST%\%~1" ^
+  || SET /A "_EXIT_CODE+=1"
+EXIT /B
+
+::复制FOLDER文件
+:COPY_FOLDER
+IF EXIST "%_CONFIG.FOLDER.DST%\%~1" EXIT /B 1
+ECHO =^> %~1
+XCOPY /E /S /I /Q /Y "%~$_CONFIG.FOLDER.SRC:1" "%_CONFIG.FOLDER.DST%\%~1" ^
+  || SET /A "_EXIT_CODE+=1"
+EXIT /B
